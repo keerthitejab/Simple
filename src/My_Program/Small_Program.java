413cae4ba629de4ad84d5b069a05b68fe95be696
keerthitejab <136094868+keerthitejab@users.noreply.github.com>
2024-07-30 18:20:41 +0530

    my final commit

---
+++ src/My_Program/Small_Program.java
@@ -0,0 +1,16 @@
+package My_Program;
+
+public class Small_Program {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=15;
+		
+		int c= a+b;
+		
+		System.out.println("addition of two numbers is: " +c);
+
+	}
+
+}
