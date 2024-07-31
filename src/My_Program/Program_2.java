c6ed6cc7c2ddba039562fc00974c8ce278d200dc
LENOVO <LENOVO@DESKTOP-7EJ77C3>
2024-07-31 16:11:58 +0530

    Modified commit

---
+++ src/My_Program/Program_2.java
@@ -0,0 +1,14 @@
+package My_Program;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=4;
+		int c=a-b;
+		System.out.println(c);
+		
+	}
+
+}
